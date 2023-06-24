3c79fb42596d0de09ded73a95a8fbe9aee2cb3ee
Macolister <bispo.mac@gmail.com>
2023-06-24 13:38:30 -0400

    write first sql lab

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER
+);
+
+
+-- CREATE TABLE cats (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER
+-- );
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'Yellow', 'Angy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Yellow', 'Angrier', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Brown', 'Cuddly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'F', 'White', 'Cuddlier', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'F', 'Black', 'Shy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'White', 'Domesticated', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'White', 'M', 'Angriest', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'F', 'White', 'Terrified', 1);
